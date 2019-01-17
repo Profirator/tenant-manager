b07045e387a4d98bb9146161e9e8a878d3ed044b
Francisco de la Vega <fdelavega@ficodes.com>
2019-01-17 14:27:11 +0100

    Add keyrock client tests

--- tests.py
+++ tests.py
@@ -18,18 +18,100 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 import unittest
-from unittest.mock import MagicMock
+from unittest.mock import MagicMock, call
 
 import keyrock_client
+from settings import VERIFY_REQUESTS
 
 
 class KeyrockClientTestCase(unittest.TestCase):
+
+    _x_subject_token = 'token'
+    _host = 'http://idm.docker:3000'
+    _user = 'user'
+    _passwd = 'passwd'
 
     def setUp(self):
         keyrock_client.requests = MagicMock()
 
     def test_authorize_user(self):
-        pass
+        # Mock login
+        response = MagicMock(status_code=201, headers={'x-subject-token': self._x_subject_token})
+        keyrock_client.requests.post.return_value = response
+
+        # Mock authorization request
+        user_response = MagicMock(status_code=201)
+        expected_info = {
+            'id': 'id',
+            'username': 'name'
+        }
+        user_response.json.return_value = expected_info
+
+        keyrock_client.requests.get.return_value = user_response
+
+        client = keyrock_client.KeyrockClient(self._host, self._user, self._passwd)
+        token = 'access_token'
+        info = client.authorize(token)
+
+        # validate calls
+        self.assertEqual(expected_info, info)
+
+        exp_body = {
+            'name': self._user,
+            'password': self._passwd
+        }
+        keyrock_client.requests.post.assert_called_once_with('http://idm.docker:3000/v3/auth/tokens', json=exp_body, verify=VERIFY_REQUESTS)
+        keyrock_client.requests.get.assert_called_once_with('http://idm.docker:3000/user?access_token=access_token')
+        user_response.json.assert_called_once_with()
+
+    def test_create_organization(self):
+        # Mock HTTP requests
+        login_response = MagicMock(status_code=201, headers={'x-subject-token': self._x_subject_token})
+
+        create_response = MagicMock(status_code=201)
+        organization = {
+            'organization': {
+                'id': 'org_id'
+            }
+        }
+        create_response.json.return_value = organization
+
+        role_response = MagicMock(status_code=201)
+
+        keyrock_client.requests.post.side_effect = [login_response, create_response, role_response]
+
+        client = keyrock_client.KeyrockClient(self._host, self._user, self._passwd)
+        org_id = client.create_organization('organization', 'description', 'owner')
+
+        # Verify calls
+        self.assertEqual('org_id', org_id)
+
+        headers = {
+            'X-Auth-Token': self._x_subject_token
+        }
+        exp_body = {
+            'name': self._user,
+            'password': self._passwd
+        }
+        org_body = {
+            'organization': {
+                'name': 'organization',
+                'description': 'description'
+            }
+        }
+        role_body = {
+            'user_organization_assignments': {
+                'role': 'owner',
+                'user_id': 'owner',
+                'organization_id': 'org_id'
+            }
+        }
+        post_calls = keyrock_client.requests.post.call_args_list
+        self.assertEqual([
+            call('http://idm.docker:3000/v3/auth/tokens', json=exp_body, verify=VERIFY_REQUESTS),
+            call('http://idm.docker:3000/v1/organizations', headers=headers, json=org_body, verify=VERIFY_REQUESTS),
+            call('http://idm.docker:3000/v1/organizations/org_id/users/owner/organization_roles/owner', headers=headers, json=role_body, verify=VERIFY_REQUESTS)
+        ], post_calls)
 
 
 if __name__ == "__main__":
